a43f910a23b03a261bc536c528d82fecc83fc829
Jacob Lapenna <jlapenna@usbr.gov>
2020-12-09 09:30:32 -0700

    Initial commit

---
+++ ExternalProjectFile.py
@@ -0,0 +1,10 @@
+# this file is part of the project open to external entities for collaboration
+
+# prompt external entity personnel for their name
+name = input("Enter your name: ")
+
+# prompt external entity personnel for their Organization
+org = input("Enter your organization: ")
+
+# welcome external personnel
+print(f"Hello, {name} from {org}, we welcome your collaboration!")
---
+++ README.md
@@ -0,0 +1,4 @@
+# This is the external README
+
+It should not replace the internal README.
+
